5e3a4ba4e901f1965187d9d6a1399456cbbb2c09
Muhammad Shoaib <shoaibriaze@gmail.com>
2024-10-12 11:43:46 +0500

    fit hai

---
+++ file.js
@@ -0,0 +1,10 @@
+// make class component
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+}
