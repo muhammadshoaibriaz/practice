5a425f4847f066f0c7079db10572f0073ffe017d
Muhammad Shoaib <shoaibriaze@gmail.com>
2024-10-12 14:36:18 +0500

    a new file arrived

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex-1 items-center justify-center min-h-100vh">
+      <h1 style={{ color: "red" }}>This is home page</h1>
+      <p style={{ color: "blue" }}>
+        This is home page and this is paragraph from home page{" "}
+      </p>
+    </div>
+  );
+}
